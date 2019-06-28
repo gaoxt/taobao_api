5a11c4ba8f3d8fe457244221a7a2a2f3d336f63b
gaoxt <simerpc@gmail.com>
2019-06-28 14:12:14 +0800

    debug taobao_web

--- taobao_api.py
+++ taobao_api.py
@@ -376,6 +376,15 @@ def fail_sys_sleep(data):
         pass
 
 
+def get_sign_val(d):
+    _m_h5_tk = re.findall(r"_m_h5_tk=([^;]*)", user_cookie)[0]
+    t = str(int(time.time() * 1000))
+    token = _m_h5_tk.split('_')[0]
+    str_sign = '&'.join([token, t, appKey, str(d)])
+    sign = hashlib.md5(str_sign.encode('utf-8')).hexdigest()
+    return sign, t
+
+
 def build_order(buyNow):
     flag = False
     sign, t = get_sign_val(buyNow)
@@ -411,15 +420,6 @@ def build_order(buyNow):
         flag = True
 
     return flag, data
-
-
-def get_sign_val(d):
-    _m_h5_tk = re.findall(r"_m_h5_tk=([^;]*)", user_cookie)[0]
-    t = str(int(time.time() * 1000))
-    token = _m_h5_tk.split('_')[0]
-    str_sign = '&'.join([token, t, appKey, str(d)])
-    sign = hashlib.md5(str_sign.encode('utf-8')).hexdigest()
-    return sign, t
 
 
 while True:
